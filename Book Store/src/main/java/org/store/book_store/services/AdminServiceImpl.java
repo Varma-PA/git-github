5310a959f9909d6ffcb1cf658aba82d5ffa9cf10
Achyuth <achyuth.varma1@gmail.com>
2019-06-16 12:15:21 +0530

    Commited Successfully

---
+++ Book Store/src/main/java/org/store/book_store/services/AdminServiceImpl.java
@@ -0,0 +1,19 @@
+package org.store.book_store.services;
+
+public class AdminServiceImpl implements IAdminService{
+
+    @Override
+    public Boolean signedIn(String userName, String sessionId) {
+        return null;
+    }
+
+    @Override
+    public Boolean verify(String userName, String password) {
+        return null;
+    }
+
+    @Override
+    public void insertSesionId(String sessionId) {
+
+    }
+}
---
+++ Book Store/src/main/java/org/store/book_store/services/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package org.store.book_store.services;
+
+import org.store.book_store.entities.BookInfo;
+
+public class BookServiceImpl implements IBookService{
+
+    @Override
+    public BookInfo addBook(BookInfo book) {
+        return null;
+    }
+
+    @Override
+    public BookInfo updateBook(BookInfo book) {
+        return null;
+    }
+
+    @Override
+    public BookInfo deleteBook(BookInfo book) {
+        return null;
+    }
+}
