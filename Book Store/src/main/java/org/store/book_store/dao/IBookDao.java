8cfa67904b826f663d6e584e200bd52e622559a3
Achyuth <achyuth.varma1@gmail.com>
2019-06-18 22:32:32 +0530

    Committed From Windows

--- Book Store/src/main/java/org/store/book_store/dao/IBookDao.java
+++ Book Store/src/main/java/org/store/book_store/dao/IBookDao.java
@@ -1,9 +1,17 @@
 package org.store.book_store.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.store.book_store.entities.BookInfo;
 
+import java.util.List;
+
 public interface IBookDao extends JpaRepository<BookInfo, Integer> {
+
+    @Query("Select BookInfo where BookInfo.bookTitle=:bookTitle")
+    List<BookInfo> listOfBooks(@Param("bookTitle") String bookTitle);
+
 
 
 
--- Book Store/src/main/java/org/store/book_store/services/BookServiceImpl.java
+++ Book Store/src/main/java/org/store/book_store/services/BookServiceImpl.java
@@ -1,21 +1,54 @@
 package org.store.book_store.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.store.book_store.controllers.AdminRestController;
+import org.store.book_store.dao.IBookDao;
 import org.store.book_store.entities.BookInfo;
 
 public class BookServiceImpl implements IBookService{
 
+    private static final Logger Log = LoggerFactory.getLogger(BookServiceImpl.class);
+
+
+    @Autowired
+    IBookDao bookDao;
+
+    public BookServiceImpl(){
+
+    }
+
+
     @Override
     public BookInfo addBook(BookInfo book) {
-        return null;
+
+        Log.info("Inside Add Book Method");
+
+        book = bookDao.save(book);
+
+
+        return book;
     }
 
     @Override
     public BookInfo updateBook(BookInfo book) {
-        return null;
+
+        Log.info("Inside Update Book");
+
+        book = bookDao.save(book);
+
+        return book;
     }
 
     @Override
     public BookInfo deleteBook(BookInfo book) {
-        return null;
+
+        Log.info("Inside Delete Book");
+
+        bookDao.delete(book);
+
+
+        return book;
     }
 }
